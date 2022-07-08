c6ce3937ca9bc6297ac37e18634cddfc82cd555e
doccomsa <newcomsa@nate.com>
2022-07-08 15:21:43 +0900

    test

--- docmall/src/main/java/com/docmall/controller/MemberController.java
+++ docmall/src/main/java/com/docmall/controller/MemberController.java
@@ -2,6 +2,7 @@ package com.docmall.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j;
@@ -16,4 +17,10 @@ public class MemberController {
 	public void join() {
 		
 	}
+	
+	@PostMapping("/join_ok")
+	public void join_ok() {
+		
+	}
+	
 }
